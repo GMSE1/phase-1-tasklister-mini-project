0c0834d02b727340d860a6dd25cf372cfc4e57ca
GMSE1 <gregory.a.marshall.jr@gmail.com>
2025-11-20 14:19:22 -0500

    Task List

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // Your code here
+  const form = document.querySelector("#create-task-form");
+  
+  function buildToDo(todo) {
+    const newTask = document.createElement("li");
+    newTask.textContent = todo;
+    return newTask;
+  }
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+    
+    const taskInput = document.querySelector("#new-task-description");
+    const taskDescription = taskInput.value;
+    
+    const todoItem = buildToDo(taskDescription);
+    
+    const taskList = document.querySelector("#tasks");
+    taskList.appendChild(todoItem);
+
+  }
+  
+  form.addEventListener("submit", handleSubmit);
 });
+
